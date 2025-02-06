470c51524d6cc0694ec66e1a55cb32fdc81a7b6a
DharmaUtomo <1439071-DharmaUtomo@users.noreply.replit.com>
2025-02-06 14:12:28 +0000

    Agent query: Please check if the web application loads correctly and if you can see the file upload section and chat interface.
    Update Langchain dependencies to include text splitters
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/b867650d-5460-44fc-a837-1c82e49166ca/74749454-9ac8-4836-945b-43805ff9b874.jpg

--- utils/text_processor.py
+++ utils/text_processor.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 import logging
